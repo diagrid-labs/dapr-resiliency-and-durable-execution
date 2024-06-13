a24f444acf7dea3656ef7fd89153cf3cf6f4290c
Marc Duiker <marcduiker@users.noreply.github.com>
2024-06-13 18:04:14 +0200

    Add first demo app and images

---
+++ ResiliencyDemo/ResiliencyDemoApp/Program.cs
@@ -0,0 +1,32 @@
+using Dapr.Client;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<DaprClient>(new DaprClientBuilder().Build());
+var app = builder.Build();
+
+const string StateStoreName = "mystatestore";
+
+app.MapPost("/social", async (
+    SocialProfileDetails details,
+    DaprClient daprClient) => {
+    await daprClient.SaveStateAsync<SocialProfileDetails>(
+        StateStoreName,
+        details.GitHubHandle,
+        details);
+
+    return Results.Created();
+});
+
+app.MapGet("/social/{key}", async (
+    string key,
+    DaprClient daprClient) => {
+    var details =  await daprClient.GetStateAsync<SocialProfileDetails>(
+        StateStoreName,
+        key);
+
+    return details is not null ? Results.Ok(details) : Results.NotFound();
+});
+
+app.Run();
+
+record SocialProfileDetails(string Name, string TwitterHandle, string GitHubHandle);

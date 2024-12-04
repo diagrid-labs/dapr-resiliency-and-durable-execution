6007930b6ccf4c1c62a6f961da7bb239c0840d4e
Marc Duiker <marcduiker@users.noreply.github.com>
2024-12-04 11:55:52 +0000

    Build projects

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,2 +1,6 @@
 dapr uninstall
 dapr init
+dotnet build ResiliencyDemo/AppA/AppA.csproj
+dotnet build ResiliencyDemo/AppA/AppB.csproj
+dotnet build WorkflowDemo/ShippingApp/ShippingApp.csproj
+dotnet build WorkflowDemo/WorkflowApp/WorkflowApp.csproj
